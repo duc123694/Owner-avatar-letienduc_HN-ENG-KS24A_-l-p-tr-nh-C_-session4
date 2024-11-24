0473cce07efba17c9be9a20ce72b6dab22bbe30f
duc123694 <tienducle86@gmail.com>
2024-11-24 21:28:02 +0700

    upload bai tap session4-1

---
+++ session4.Ex1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Moi nhap so",a);
+	scanf("%d",&a);
+	if(a > 0){
+		printf("Day la so duong");
+	}
+	else{
+		printf("Day la so am");
+	}
+	
+	return 0;
+}
